b422fbf870f5a2561ab6e63f8484db6adef6225f
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-01 18:43:51 +0100

    Refactor RainbowKitProvider and add app config
    Updated RainbowKitProvider import and added default config for Afrodex Staking DApp.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,16 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import config from '../wagmi';
+import { mainnet } from 'wagmi/chains';
+
+const config = getDefaultConfig({
+  appName: 'Afrodex Staking DApp',
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+  chains: [mainnet],
+  ssr: true,
+});
 
 const queryClient = new QueryClient();
 
