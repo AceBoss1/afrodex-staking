3127cd166c8a98353e19657650f5a81cb1ed988e
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-01 16:41:23 +0100

    Refactor AfrodexStaking component
    Refactor AfrodexStaking component to streamline imports and state management. Update contract interaction logic and improve UI structure.

--- src/components/AfrodexStaking.jsx
+++ src/components/AfrodexStaking.jsx
@@ -1,117 +1,54 @@
-import { STAKING_ABI, AFRODEX_TOKEN_ABI } from '@/lib/abis';
+'use client';
 import { useState, useEffect } from 'react';
+import { useAccount, useContractRead, useContractWrite, usePrepareContractWrite } from 'wagmi';
 import { ethers } from 'ethers';
-import { useAccount, useConnect, useDisconnect, useSigner } from 'wagmi';
-import { Web3Button } from '@web3modal/react';
+import { STAKING_ABI, AFRODEX_TOKEN_ABI } from '../lib/abis';
 
 
-const STAKING_ABI = [[{"constant":false,"inputs":[{"name":"tokenGet","type":"address"},{"name":"amountGet","type":"uint256"},{"name":"tokenGive","type":"address"},{"name":"amountGive","type":"uint256"},{"name":"expires","type":"uint256"},{"name":"nonce","type":"uint256"},{"name":"user","type":"address"},{"name":"v","type":"uint8"},{"name":"r","type":"bytes32"},{"name":"s","type":"bytes32"},{"name":"amount","type":"uint256"}],"name":"trade","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"tokenGet","type":"address"},{"name":"amountGet","type":"uint256"},{"name":"tokenGive","type":"address"},{"name":"amountGive","type":"uint256"},{"name":"expires","type":"uint256"},{"name":"nonce","type":"uint256"}],"name":"order","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"bytes32"}],"name":"orderFills","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"tokenGet","type":"address"},{"name":"amountGet","type":"uint256"},{"name":"tokenGive","type":"address"},{"name":"amountGive","type":"uint256"},{"name":"expires","type":"uint256"},{"name":"nonce","type":"uint256"},{"name":"v","type":"uint8"},{"name":"r","type":"bytes32"},{"name":"s","type":"bytes32"}],"name":"cancelOrder","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"token","type":"address"},{"name":"amount","type":"uint256"}],"name":"depositToken","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"tokenGet","type":"address"},{"name":"amountGet","type":"uint256"},{"name":"tokenGive","type":"address"},{"name":"amountGive","type":"uint256"},{"name":"expires","type":"uint256"},{"name":"nonce","type":"uint256"},{"name":"user","type":"address"},{"name":"v","type":"uint8"},{"name":"r","type":"bytes32"},{"name":"s","type":"bytes32"}],"name":"amountFilled","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"tokens","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"feeMake_","type":"uint256"}],"name":"changeFeeMake","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"feeMake","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"feeRebate_","type":"uint256"}],"name":"changeFeeRebate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"feeAccount","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"tokenGet","type":"address"},{"name":"amountGet","type":"uint256"},{"name":"tokenGive","type":"address"},{"name":"amountGive","type":"uint256"},{"name":"expires","type":"uint256"},{"name":"nonce","type":"uint256"},{"name":"user","type":"address"},{"name":"v","type":"uint8"},{"name":"r","type":"bytes32"},{"name":"s","type":"bytes32"},{"name":"amount","type":"uint256"},{"name":"sender","type":"address"}],"name":"testTrade","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"feeAccount_","type":"address"}],"name":"changeFeeAccount","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"feeRebate","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"feeTake_","type":"uint256"}],"name":"changeFeeTake","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"admin_","type":"address"}],"name":"changeAdmin","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"token","type":"address"},{"name":"amount","type":"uint256"}],"name":"withdrawToken","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"bytes32"}],"name":"orders","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"feeTake","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"accountLevelsAddr_","type":"address"}],"name":"changeAccountLevelsAddr","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"accountLevelsAddr","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"token","type":"address"},{"name":"user","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"admin","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"tokenGet","type":"address"},{"name":"amountGet","type":"uint256"},{"name":"tokenGive","type":"address"},{"name":"amountGive","type":"uint256"},{"name":"expires","type":"uint256"},{"name":"nonce","type":"uint256"},{"name":"user","type":"address"},{"name":"v","type":"uint8"},{"name":"r","type":"bytes32"},{"name":"s","type":"bytes32"}],"name":"availableVolume","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"admin_","type":"address"},{"name":"feeAccount_","type":"address"},{"name":"accountLevelsAddr_","type":"address"},{"name":"feeMake_","type":"uint256"},{"name":"feeTake_","type":"uint256"},{"name":"feeRebate_","type":"uint256"}],"payable":false,"type":"constructor"},{"payable":false,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":false,"name":"tokenGet","type":"address"},{"indexed":false,"name":"amountGet","type":"uint256"},{"indexed":false,"name":"tokenGive","type":"address"},{"indexed":false,"name":"amountGive","type":"uint256"},{"indexed":false,"name":"expires","type":"uint256"},{"indexed":false,"name":"nonce","type":"uint256"},{"indexed":false,"name":"user","type":"address"}],"name":"Order","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"tokenGet","type":"address"},{"indexed":false,"name":"amountGet","type":"uint256"},{"indexed":false,"name":"tokenGive","type":"address"},{"indexed":false,"name":"amountGive","type":"uint256"},{"indexed":false,"name":"expires","type":"uint256"},{"indexed":false,"name":"nonce","type":"uint256"},{"indexed":false,"name":"user","type":"address"},{"indexed":false,"name":"v","type":"uint8"},{"indexed":false,"name":"r","type":"bytes32"},{"indexed":false,"name":"s","type":"bytes32"}],"name":"Cancel","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"tokenGet","type":"address"},{"indexed":false,"name":"amountGet","type":"uint256"},{"indexed":false,"name":"tokenGive","type":"address"},{"indexed":false,"name":"amountGive","type":"uint256"},{"indexed":false,"name":"get","type":"address"},{"indexed":false,"name":"give","type":"address"}],"name":"Trade","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"token","type":"address"},{"indexed":false,"name":"user","type":"address"},{"indexed":false,"name":"amount","type":"uint256"},{"indexed":false,"name":"balance","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"token","type":"address"},{"indexed":false,"name":"user","type":"address"},{"indexed":false,"name":"amount","type":"uint256"},{"indexed":false,"name":"balance","type":"uint256"}],"name":"Withdraw","type":"event"}]];
-const ERC20_ABI = [[{"constant":false,"inputs":[{"name":"newImplementation","type":"address"}],"name":"upgradeTo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newImplementation","type":"address"},{"name":"data","type":"bytes"}],"name":"upgradeToAndCall","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[],"name":"implementation","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newAdmin","type":"address"}],"name":"changeAdmin","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"admin","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_logic","type":"address"},{"name":"_admin","type":"address"},{"name":"_data","type":"bytes"}],"payable":true,"stateMutability":"payable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":false,"name":"previousAdmin","type":"address"},{"indexed":false,"name":"newAdmin","type":"address"}],"name":"AdminChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"implementation","type":"address"}],"name":"Upgraded","type":"event"}]];
-
-
-const stakingABI = STAKING_ABI;
-const tokenABI = ERC20_ABI;
+const stakingAddress = process.env.NEXT_PUBLIC_STAKING_CONTRACT_ADDRESS;
+const tokenAddress = process.env.NEXT_PUBLIC_AFRODEX_TOKEN_ADDRESS;
 
 
 export default function AfrodexStaking() {
-const [amount, setAmount] = useState('');
-const [allowance, setAllowance] = useState('0');
-const [balance, setBalance] = useState('0');
-const [status, setStatus] = useState('');
-
-
 const { address, isConnected } = useAccount();
-const { data: signer } = useSigner();
+const [amount, setAmount] = useState('');
 
 
-const tokenAddress = process.env.NEXT_PUBLIC_AFRODEX_TOKEN_ADDRESS;
-const stakingAddress = process.env.NEXT_PUBLIC_STAKING_CONTRACT_ADDRESS;
+const { config: depositConfig } = usePrepareContractWrite({
+address: stakingAddress,
+abi: STAKING_ABI,
+functionName: 'depositToken',
+args: [tokenAddress, ethers.parseUnits(amount || '0', 18)],
+enabled: Boolean(isConnected && amount),
+});
 
 
-async function loadData() {
-if (!signer || !address) return;
-const token = new ethers.Contract(tokenAddress, tokenABI, signer);
-const allowance = await token.allowance(address, stakingAddress);
-const balance = await token.balanceOf(address);
-setAllowance(ethers.utils.formatUnits(allowance, 18));
-setBalance(ethers.utils.formatUnits(balance, 18));
-}
-
-
-async function approve() {
-if (!signer || !amount) return;
-const token = new ethers.Contract(tokenAddress, tokenABI, signer);
-const tx = await token.approve(stakingAddress, ethers.utils.parseUnits(amount, 18));
-setStatus('Approving...');
-await tx.wait();
-setStatus('Approved!');
-loadData();
-}
-
-
-async function stake() {
-if (!signer || !amount) return;
-const contract = new ethers.Contract(stakingAddress, stakingABI, signer);
-const tx = await contract.depositToken(tokenAddress, ethers.utils.parseUnits(amount, 18));
-setStatus('Staking...');
-await tx.wait();
-setStatus('Staked Successfully!');
-loadData();
-}
-
-
-async function withdraw() {
-if (!signer || !amount) return;
-const contract = new ethers.Contract(stakingAddress, stakingABI, signer);
-const tx = await contract.withdrawToken(tokenAddress, ethers.utils.parseUnits(amount, 18));
-setStatus('Withdrawing...');
-await tx.wait();
-setStatus('Withdrawn Successfully!');
-loadData();
-}
-
-
-useEffect(() => {
-if (isConnected) loadData();
-}, [isConnected]);
+const { write: deposit, isLoading: depositing } = useContractWrite(depositConfig);
 
 
 return (
-<div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center p-8">
-<div className="bg-gray-800 rounded-2xl shadow-lg p-8 w-full max-w-md text-center">
-<h1 className="text-3xl font-bold mb-4">Afrodex Staking</h1>
-<p className="text-gray-400 mb-4">Stake AFRODEX tokens and earn rewards.</p>
-
-
-{!isConnected ? (
-<Web3Button icon="show" label="Connect Wallet" balance="show" />
-) : (
-<>
-<p className="mb-2">Connected: {address.slice(0, 6)}...{address.slice(-4)}</p>
-<p className="text-sm text-gray-400 mb-2">Balance: {balance} AFRODEX</p>
-<p className="text-sm text-gray-400 mb-4">Allowance: {allowance} AFRODEX</p>
-
-
+<div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+<h1 className="text-3xl font-bold mb-6">Afrodex Staking Dashboard</h1>
+{isConnected ? (
+<div className="w-full max-w-md p-6 bg-gray-800 rounded-2xl shadow-lg">
 <input
 type="number"
-placeholder="Enter amount"
 value={amount}
 onChange={(e) => setAmount(e.target.value)}
-className="w-full p-2 rounded-lg text-black mb-4"
+placeholder="Amount to stake"
+className="w-full mb-4 p-3 rounded bg-gray-700 text-white"
 />
-
-
-<div className="flex flex-col gap-3">
-<button onClick={approve} className="bg-blue-600 hover:bg-blue-700 p-3 rounded-lg">Approve</button>
-<button onClick={stake} className="bg-green-600 hover:bg-green-700 p-3 rounded-lg">Stake</button>
-<button onClick={withdraw} className="bg-red-600 hover:bg-red-700 p-3 rounded-lg">Withdraw</button>
+<button
+onClick={() => deposit?.()}
+disabled={!deposit || depositing}
+className="w-full py-3 rounded bg-indigo-600 hover:bg-indigo-700 disabled:bg-gray-500"
+>
+{depositing ? 'Staking...' : 'Stake Tokens'}
+</button>
 </div>
-
-
-{status && <p className="text-green-400 mt-4">{status}</p>}
-</>
+) : (
+<p className="text-gray-400">Please connect your wallet to continue.</p>
 )}
-</div>
 </div>
 );
 }
