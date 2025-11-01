17e0153f75c877f9b34de826f2dc9a4bcf042686
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-01 17:43:33 +0100

    Refactor imports in _app.js
    Removed unused getDefaultConfig and mainnet imports. Updated config import from local wagmi file.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,9 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { mainnet } from 'wagmi/chains';
-
-const config = getDefaultConfig({
-  appName: 'Afrodex Staking DApp',
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
-  chains: [mainnet],
-  ssr: true,
-});
+import config from '../wagmi';
 
 const queryClient = new QueryClient();
 
