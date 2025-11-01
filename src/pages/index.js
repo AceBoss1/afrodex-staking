dee7d68b940df4f958644c19d9c37d20aec7df9f
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-01 16:18:02 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,18 @@
+import Head from 'next/head';
 import AfrodexStaking from '../components/AfrodexStaking';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Afrodex Staking Platform</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Welcome to Afrodex</h1>
+        <AfrodexStaking />
+      </main>
+    </div>
+  );
+}
