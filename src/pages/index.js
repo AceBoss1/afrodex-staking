af5859a61772dfec9515897c48cf0452e95528a9
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-01 16:48:52 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
+import Head from 'next/head';
 import AfrodexStaking from '../components/AfrodexStaking';
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
-      <h1 className="text-4xl font-extrabold text-afrodexBlue mb-8">
-        Welcome to Afrodex Staking
-      </h1>
-      <AfrodexStaking />
-    </main>
+    <>
+      <Head>
+        <title>Afrodex Staking</title>
+      </Head>
+      <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+        <h1 className="text-4xl font-extrabold text-afrodexBlue mb-8">
+          Welcome to Afrodex Staking
+        </h1>
+        <AfrodexStaking />
+      </main>
+    </>
   );
-}
+};
+
+export default Home;
