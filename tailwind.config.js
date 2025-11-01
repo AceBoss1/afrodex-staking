163cdfa659fe4800a999c0f3f2520012faa0b5ab
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-01 16:23:57 +0100

    Modify Tailwind config for new structure and colors
    Updated content paths and extended theme with custom color.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-theme: { extend: {} },
-plugins: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        afrodexBlue: "#0d76fc",
+      },
+    },
+  },
+  plugins: [],
 };
