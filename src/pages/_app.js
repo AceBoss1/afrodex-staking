235e94ab42dfdf94c8c1617cbfb75731c0c58018
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-02 23:22:59 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 /** src/pages/_app.js */
-import '@/styles/globals.css';
+import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
