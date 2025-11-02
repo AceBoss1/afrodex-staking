fd80700052d0500edf2d8ba268a4abf9021b4e43
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-02 19:34:48 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,24 @@
 /** src/pages/_app.js */
-
 import '@/styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { WagmiConfig } from 'wagmi';
+import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import { wagmiConfig, chains } from '../wagmi';
+import wagmiConfig, { chains } from '../wagmi';
 
-// Create Query Client (Fixes: `No QueryClient set`)
+// ✅ Fixes: "No QueryClient set"
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
   return (
-    <WagmiConfig config={wagmiConfig}>
+    <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider chains={chains}>
           <Component {...pageProps} />
         </RainbowKitProvider>
       </QueryClientProvider>
-    </WagmiConfig>
+    </WagmiProvider>
   );
 }
