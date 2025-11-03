79c51051f56e65f9ed38f15421ed09913d30150a
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-03 18:31:14 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,17 +6,21 @@ import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { mainnet } from 'wagmi/chains';
 
+const queryClient = new QueryClient();
+
+const alchemyKey = process.env.NEXT_PUBLIC_ALCHEMY_KEY;
+const wcProjectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
+
 const config = getDefaultConfig({
   appName: 'Afrodex Staking DApp',
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+  projectId: wcProjectId,
   chains: [mainnet],
   transports: {
-    [mainnet.id]: `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
+    [mainnet.id]: http(`https://eth-mainnet.g.alchemy.com/v2/${alchemyKey}`),
   },
   ssr: true,
 });
 
-const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
   return (
