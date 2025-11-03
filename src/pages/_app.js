efcc0404101468fe1a10f53e2121eb9bf34efa07
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-03 19:43:03 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,21 @@
-// src/pages/_app.js
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+
+import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { mainnet } from 'wagmi/chains';
+import { http } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
+const alchemyKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
 
 const config = getDefaultConfig({
-  appName: 'Afrodex Staking DApp',
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+  appName: 'AfroDex Staking',
+  projectId,
   chains: [mainnet],
   transports: {
-    [mainnet.id]: `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
+    [mainnet.id]: http(`https://eth-mainnet.g.alchemy.com/v2/${alchemyKey}`),
   },
   ssr: true,
 });
