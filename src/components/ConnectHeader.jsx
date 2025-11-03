e59471e9aef0deb61b739c21d98aa15fad6be2b6
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-03 23:21:17 +0100

    Update ConnectHeader.jsx

--- src/components/ConnectHeader.jsx
+++ src/components/ConnectHeader.jsx
@@ -8,16 +8,17 @@ export default function ConnectHeader() {
   return (
     <header className="w-full py-8">
       <div className="max-w-2xl mx-auto text-center">
-        <h2 className="text-lg text-gray-300 mb-4">Welcome to Afrodex Staking</h2>
+        <h2 className="text-lg text-gray-300 mb-4">Staking Powered by AfroDex Communinity of Trust & AfroDex Ambassadors</h2>
 
         <div className="flex items-center justify-center my-2">
           <ConnectButton label="Connect Wallet" />
         </div>
 
-      <h1 className="mt-6 text-3xl md:text-4xl font-extrabold text-orange-500 pulse-flame">
-
-          AfroX Staking Dashboard
-        </h1>
+<h1 className="text-orange-400 text-3xl font-bold flex items-center gap-3">
+  <img src="/afrodex_logoT.png" className="h-8 w-auto" />
+  AfroX Staking and Minting Engine
+  <img src="/afrodex_logoA.png" className="h-8 w-auto" />
+</h1>
       </div>
     </header>
   );
