95fab752797ce3b45fee9b9e6a1223c8b52c8696
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-03 19:30:09 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,31 @@
 // src/pages/_app.js
 import '../styles/globals.css';
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { mainnet } from 'wagmi/chains';
-import { http } from 'wagmi';
-
-const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
-const alchemyKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
 
 const config = getDefaultConfig({
-  appName: 'AfroDex Staking',
-  projectId,
+  appName: 'Afrodex Staking DApp',
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
   chains: [mainnet],
   transports: {
-    [mainnet.id]: http(`https://eth-mainnet.g.alchemy.com/v2/${alchemyKey}`)
+    [mainnet.id]: `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
   },
   ssr: true,
 });
 
+const queryClient = new QueryClient();
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
