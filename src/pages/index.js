5850637ec5f49a92da7c4318ffd528aae86c514f
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-10 22:52:16 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import dynamic from 'next/dynamic';
 import ConnectHeader from '../components/ConnectHeader';
 const AfrodexStaking = dynamic(() => import('../components/AfrodexStaking'), { ssr: false });
 
+export const metadata = {
+  title: "AfroX Staking and Minting Engine"
+};
+
 export default function Home() {
   return (
     <>
