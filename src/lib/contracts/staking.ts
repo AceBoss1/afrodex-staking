5ceb8eea751e9d79b07813fb020a33d3a314b73d
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-16 14:23:31 +0100

    Create staking.ts

---
+++ src/lib/contracts/staking.ts
@@ -0,0 +1,31 @@
+// src/lib/contracts/staking.ts
+export const stakingABI = [
+  {
+    "inputs": [],
+    "name": "getStake",
+    "outputs": [{ "name": "", "type": "uint256" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getReward",
+    "outputs": [{ "name": "", "type": "uint256" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "name": "amount", "type": "uint256" }],
+    "name": "stake",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "claimReward",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
