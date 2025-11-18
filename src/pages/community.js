a53851a98f780c5d9842d9dd9084115915de6245
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-18 01:33:29 +0100

    Create community.js

---
+++ src/pages/community.js
@@ -0,0 +1,11 @@
+import ConnectHeader from "../src/components/ConnectHeader";
+import GovernanceDashboard from "../src/components/GovernanceDashboard";
+
+export default function CommunityPage() {
+  return (
+    <>
+      <ConnectHeader />
+      <GovernanceDashboard />
+    </>
+  );
+}
