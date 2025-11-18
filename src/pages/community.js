c2e65ea5b4188b1d4c1fe7beb7a2d85b4a4fe0fa
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-18 01:49:21 +0100

    Update community.js

--- src/pages/community.js
+++ src/pages/community.js
@@ -1,11 +1,15 @@
-import ConnectHeader from "../src/components/ConnectHeader";
-import GovernanceDashboard from "../src/components/GovernanceDashboard";
+import dynamic from "next/dynamic";
+
+const ConnectHeader = dynamic(() => import("../components/ConnectHeader"), { ssr: false });
 
 export default function CommunityPage() {
   return (
     <>
       <ConnectHeader />
-      <GovernanceDashboard />
+      <div style={{ padding: 40, textAlign: "center" }}>
+        <h1>Community Dashboard Coming Soon</h1>
+        <p>Feature will be activated after staking dashboard is complete.</p>
+      </div>
     </>
   );
 }
