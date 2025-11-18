c8cd682e43e356531347f762370cea9367255f86
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-18 01:33:07 +0100

    Create ambassador.js

---
+++ src/pages/ambassador.js
@@ -0,0 +1,11 @@
+import ConnectHeader from "../src/components/ConnectHeader";
+import AmbassadorDashboard from "../src/components/AmbassadorDashboard";
+
+export default function AmbassadorPage() {
+  return (
+    <>
+      <ConnectHeader />
+      <AmbassadorDashboard />
+    </>
+  );
+}
