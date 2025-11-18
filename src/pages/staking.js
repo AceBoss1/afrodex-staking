d87af4fc4847f3c7656d14e6614f00c11afcf578
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-18 01:32:42 +0100

    Create staking.js

---
+++ src/pages/staking.js
@@ -0,0 +1,11 @@
+import ConnectHeader from "../src/components/ConnectHeader";
+import AfrodexStaking from "../src/components/AfrodexStaking";
+
+export default function StakingPage() {
+  return (
+    <>
+      <ConnectHeader />
+      <AfrodexStaking />
+    </>
+  );
+}
