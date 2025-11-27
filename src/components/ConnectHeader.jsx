779ab377250d0881135f606e8a0ced94e2af403e
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-27 23:27:10 +0100

    Update ConnectHeader.jsx

--- src/components/ConnectHeader.jsx
+++ src/components/ConnectHeader.jsx
@@ -12,7 +12,7 @@ return (
 
 
 <h2 className="text-lg text-gray-300 mb-4">
-Staking Powered by AfroDex Community of Trust & AfroDex Ambassadors
+DeFi Powered by AfroDex Community of Trust & AfroDex Ambassadors
 </h2>
 
 
@@ -20,15 +20,6 @@ Staking Powered by AfroDex Community of Trust & AfroDex Ambassadors
 <ConnectButton />
 </div>
 
-
-<h1 className="text-orange-400 text-3xl font-bold flex items-center gap-4 justify-center">
-<img src="/afrodex_logoT.png" alt="T Logo" className="h-9 w-auto opacity-90" />
-AfroX Staking and Minting Engine
-<img src="/afrodex_logoA.png" alt="A Logo" className="h-9 w-auto opacity-90" />
-</h1>
-
-
-</div>
 </header>
 );
 }
